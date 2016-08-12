4f3bd11de2b3b042dec110d736876b1246a0d1b1
lferran <lferran@student.ethz.ch>
2016-08-12 15:05:31 +0000

    added flowserver_path variable

--- fibte/__init__.py
+++ fibte/__init__.py
@@ -1,8 +1,7 @@
-import inspect
 import ConfigParser
 import os
 
-RES= os.path.join(os.path.dirname(__file__),'res')
+RES = os.path.join(os.path.dirname(__file__),'res')
 
 CFG = ConfigParser.ConfigParser()
 
@@ -11,7 +10,20 @@ with open(os.path.join(RES,'config.cfg'),'r') as f:
 
 tmp_files = CFG.get("DEFAULT","tmp_files")
 db_topo = CFG.get("DEFAULT","db_topo")
+
 LINK_BANDWIDTH = float(CFG.get("DEFAULT","link_bandwidth"))
 
+MIN_MICE_SIZE = float(CFG.get("DEFAULT", "min_mice_size"))*LINK_BANDWIDTH
+MAX_MICE_SIZE = float(CFG.get("DEFAULT", "max_mice_size"))*LINK_BANDWIDTH
+MIN_ELEPHANT_SIZE = float(CFG.get("DEFAULT", "min_elephant_size"))*LINK_BANDWIDTH
+MAX_ELEPHANT_SIZE = float(CFG.get("DEFAULT", "max_elephant_size"))*LINK_BANDWIDTH
+
+MICE_SIZE_RANGE = [MIN_MICE_SIZE, MAX_MICE_SIZE]
+ELEPHANT_SIZE_RANGE = [MIN_ELEPHANT_SIZE, MAX_ELEPHANT_SIZE]
+
+MICE_SIZE_STEP = float(CFG.get("DEFAULT", "mice_size_step"))*LINK_BANDWIDTH
+ELEPHANT_SIZE_STEP = float(CFG.get("DEFAULT", "elephant_size_step"))*LINK_BANDWIDTH
+
+import inspect
 import fibte.trafficgen.flowServer
-flowServer_path= inspect.getsourcefile(fibte.trafficgen.flowServer)
+flowServer_path = inspect.getsourcefile(fibte.trafficgen.flowServer)
