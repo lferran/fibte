1c924ed2a1122b3fc413a4676de62dc75f42aa91
lferran <lferran@student.ethz.ch>
2016-08-13 16:21:08 +0000

    implemented function that generates random datacenter dag given a prefix

--- fibte/loadbalancer.py
+++ fibte/loadbalancer.py
@@ -9,7 +9,7 @@ import time
 import argparse
 import json
 import networkx as nx
-
+import random
 from fibte.misc.topologyGraph import TopologyGraph
 
 from fibte.monitoring.getLoads import GetLoads
@@ -90,13 +90,14 @@ class LBController(object):
         # Receive network graph
         self.network_graph = self.sbmanager.igp_graph
 
+
         # Create my modified version of the graph
         self.dc_graph = self._addDCInfoToGraph(self.network_graph.copy())
 
-        # Here we store the current dags for each destiantion
-        self.dags = {}
 
-        import ipdb; ipdb.set_trace()
+        # Here we store the current dags for each destiantion
+        self.dags = self._createInitialDags()
+
         # Start getLoads thread that reads from counters
         os.system(getLoads_path + ' -k {0} &'.format(self.k))
 
@@ -107,6 +108,11 @@ class LBController(object):
 
     def getRouterPod(self, router):
         return self.dc_graph.node[router]['pod']
+
+    def getAllRouters(self):
+        return [router for router in self.dc_graph.routers
+                if any([True for type in ['edge', 'aggregation', 'core']
+                        if type in self.dc_graph.node[router].keys()])]
 
     def getEdgeRouters(self, pod=-1):
         if pod == -1:
@@ -124,7 +130,7 @@ class LBController(object):
         return [router for router in self.dc_graph.routers if self.dc_graph.node[router].has_key('core')]
 
     def getConnectedCoreRouters(self, aggregationRouter):
-        return [r for r in self.getCoreRouters() if self.dc_graph[aggregationRouter].hay_key(r)]
+        return [r for r in self.getCoreRouters() if self.dc_graph[aggregationRouter].has_key(r)]
 
     def getConnectedAggregation(self, coreRouter, pod=-1):
         """
@@ -136,11 +142,12 @@ class LBController(object):
         """
         if pod == -1:
             return [r for r in self.dc_graph[coreRouter].keys() if
-                    self.dc_graph[coreRouter][r]['direction'] == 'downlink']
+                    self.dc_graph.is_router(r) and self.dc_graph[coreRouter][r]['direction'] == 'downlink']
         else:
-            return [r for r in self.dc_graph[coreRouter].keys() if
-                    self.dc_graph[coreRouter][r]['direction'] == 'downlink'
-                    and self.getRouterPod(r) == pod]
+            return [r for r in self.dc_graph[coreRouter].keys()
+                    if self.dc_graph.is_router(r)
+                    and self.dc_graph[coreRouter][r]['direction'] == 'downlink'
+                    and self.getRouterPod(r) == pod][0]
 
     def getGatewayRouter(self, prefix):
         """
@@ -153,6 +160,39 @@ class LBController(object):
             return [edgeRouter for edgeRouter in edgeRouters if self.dc_graph[edgeRouter].has_key(prefix)][0]
         else:
             raise ValueError
+
+    def getRouterFromIndex(self, type, index, pod=-1):
+        """
+        Given the router type, index and optionally pod number,
+        returns the corresponding router id
+
+        :param type: edge, aggregation or core
+        :param index:
+        :param pod:
+        :return:
+        """
+        if type == 'core' and pod == -1:
+            if (index >= 0 and index <= ((self.k**2)/4-1)):
+                template = "r_c{0}".format(index)
+                return self.topology.routersIdMapping["nameToId"][template]
+            else:
+                raise ValueError("Index out of core router range: 0 =< index =< k**2/4 -1")
+
+        elif pod != -1:
+            if index >= 0 and index <= ((self.k/2)-1):
+                if type == 'aggregation':
+                    template = "r_{0}_a{1}".format(pod, index)
+                    return self.topology.routersIdMapping["nameToId"][template]
+
+                elif type == 'edge':
+                    template = "r_{0}_e{1}".format(pod, index)
+                    return self.topology.routersIdMapping["nameToId"][template]
+
+            else:
+                raise ValueError("Index out of {0} router range: 0 =< index =< k**2/4 -1".format(type))
+
+        else:
+            raise ValueError("Wrong router type: {0}".format(type))
 
     def getConnectedPrefixes(self, edgeRouter):
         """
@@ -170,35 +210,43 @@ class LBController(object):
         # Now iterate router links and say if they are uplink or downlink
         visited = []
         for (u,v) in graph.router_links:
+
             if (u, v) not in visited and (v, u) not in visited:
                 uname = self.topology.getRouterName(u)
                 utype = self.topology.getRouterType(uname)
                 vname = self.topology.getRouterName(v)
                 vtype = self.topology.getRouterType(vname)
 
-                upod = self.topology.getRouterPod(uname)
-                vpod = self.topology.getRouterPod(vname)
-                graph.node[u]['pod'] = upod
-                graph.node[v]['pod'] = vpod
+                # Add pod number if not core
+                if utype != 'core':
+                    upod = self.topology.getRouterPod(uname)
+                    graph.node[u]['pod'] = upod
+                if vtype != 'core':
+                    vpod = self.topology.getRouterPod(vname)
+                    graph.node[v]['pod'] = vpod
 
+                # Case uplink from edge to aggregation
                 if utype == 'edge' and vtype == 'aggregation':
                     graph.node[u]['edge'] = True
                     graph.node[v]['aggregation'] = True
                     graph.edge[u][v]['direction'] = 'uplink'
                     graph.edge[v][u]['direction'] = 'downlink'
 
+                # Case downling from aggregation to edge
                 elif utype == 'aggregation' and vtype == 'edge':
                     graph.node[u]['aggregation'] = True
                     graph.node[v]['edge'] = True
                     graph.edge[u][v]['direction'] = 'downlink'
                     graph.edge[v][u]['direction'] = 'uplink'
 
+                # Case downling from core to aggregation
                 elif utype == 'core' and vtype == 'aggregation':
                     graph.node[u]['core'] = True
                     graph.node[v]['aggregation'] = True
                     graph.edge[u][v]['direction'] = 'downlink'
                     graph.edge[v][u]['direction'] = 'uplink'
 
+                # Case upling aggregation to core
                 elif utype == 'aggregation' and vtype == 'core':
                     graph.node[u]['aggregation'] = True
                     graph.node[v]['core'] = True
@@ -209,11 +257,23 @@ class LBController(object):
 
         return graph
 
+    def _getPrintableDag(self, dag):
+        dag_c = nx.DiGraph()
+        for (u,v) in dag.edges_iter():
+            dag_c.add_edge(self.topology.getRouterName(u), self.topology.getRouterName(v))
+        return dag_c
+
+    def _printRouterName(self, router):
+        print self.topology.getRouterName(router)
+
     def _createInitialDags(self):
         """
         Populates the self.dags dictionary for each existing prefix in the network
 
         """
+        # Result is stored here
+        dags = {}
+
         # Get edge routers
         edgeRouters = self.getEdgeRouters()
 
@@ -223,8 +283,9 @@ class LBController(object):
         # Get Core routers
         coreRouters = self.getCoreRouters()
 
-        for prefix in self.network_graph.prefix:
 
+        for prefix in self.network_graph.prefixes:
+            # Create a new dag
             dag = nx.DiGraph()
 
             # Get Edge router connected to prefix
@@ -258,10 +319,107 @@ class LBController(object):
             # Add downlinks core -> pod
             for coreRouter in coreRouters:
                 # Only downling from core router to aggregation router of specific pod
-                action = [dag.add_edge(coreRouter, aR) for aR in  self.getConnectedAggregation(pod=pod)]
+                action = [dag.add_edge(coreRouter, aR) for aR in  self.getConnectedAggregation(coreRouter, pod=pod)]
 
             # Add dag
-            self.dags[prefix] = dag
+            dags[prefix] = dag
+
+        return dags
+
+    def getRandomUpLinkChoice(self, max_depth=2, curr_depth=0):
+        if curr_depth == max_depth:
+            return []
+
+        else:
+            if curr_depth == 0:
+                # Draw how many
+                n = random.randint(1, self.k/2)
+
+                # Draw which
+                indexes = range(0, self.k/2)
+                random.shuffle(indexes)
+                chosen = indexes[:n]
+
+            elif curr_depth == 1:
+                # Draw how many
+                n = random.randint(1, (self.k**2/4))
+
+                # Draw which
+                indexes = range(0, (self.k**2/4))
+                random.shuffle(indexes)
+                chosen = indexes[:n]
+
+            new_current_depth = curr_depth + 1
+            return [(c, self.getRandomUpLinkChoice(max_depth, curr_depth=new_current_depth)) for c in chosen]
+
+    def getRandomDag(self, prefix):
+        """
+        Returns a random dag (upwards) from every
+        other prefix to specified target prefix.
+
+        :param prefix:
+        :return:
+        """
+        # Result is stored here
+        rdag = nx.DiGraph()
+
+        # Get first the gateway edge router of the prefix
+        gwRouter = self.getGatewayRouter(prefix)
+
+        # Get pod number
+        prefixPod = self.getRouterPod(gwRouter)
+
+        # Then starting at every other edge
+        for router in self.getEdgeRouters():
+
+            # Get pod of the router
+            rPod = self.getRouterPod(router)
+
+            # If they are in different pods
+            if rPod != prefixPod:
+
+                # Make uplink choices
+                choices = self.getRandomUpLinkChoice(max_depth=2)
+
+                # Add corresponding edges
+                for choice in choices:
+                    # Fetch aggregation router chosen
+                    aggChosen = self.getRouterFromIndex(type='aggregation', index=choice[0], pod=rPod)
+
+                    # Add the edge from edge router to randomly chosen aggregation router
+                    rdag.add_edge(router, aggChosen)
+
+                    # Traverse now the core router choices from each aggregation
+                    for subchoice in choice[1]:
+                        # Fetch core router
+                        coreRouter = self.getRouterFromIndex(type='core', index=subchoice[0])
+
+                        # Add edge aggregation -> core
+                        rdag.add_edge(aggChosen, coreRouter)
+
+                        # Add also here the downlink edge core -> aggregation
+                        corrAggr = self.getConnectedAggregation(coreRouter=coreRouter, pod=prefixPod)
+                        rdag.add_edge(coreRouter, corrAggr)
+
+            # Edge router in the same pod
+            else:
+                # Check that it's not himself
+                if router != gwRouter:
+                    # Draw also the used links (inside pod)
+                    choices = self.getRandomUpLinkChoice(max_depth=1)
+
+                    # Add corresponding edges
+                    for choice in choices:
+                        # Fetch aggregation router chosen
+                        aggChosen = self.getRouterFromIndex(type='aggregation', index=choice[0], pod=rPod)
+
+                        # Add the edge from edge router to randomly chosen aggregation router
+                        rdag.add_edge(router, aggChosen)
+
+                        # Add also the downlink fixed link: aggregation -> edge router
+                        rdag.add_edge(aggChosen, gwRouter)
+
+        return rdag
 
     def reset(self):
         pass
@@ -355,6 +513,11 @@ if __name__ == '__main__':
 
 
     lb = LBController(doBalance = args.doBalance, k=args.k)
+
+    prefix = lb.dags.keys()[3]
+    pdag = lb.getRandomDag(prefix)
+    import ipdb; ipdb.set_trace()
+
     lb.run()
 
     #import ipdb; ipdb.set_trace()
