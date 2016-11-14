d6ad95e7b91f47d6274bf6eee621c5c46457853b
Ferran Llamas <lferran@student.ethz.ch>
2016-11-14 15:52:45 +0000

    improved TCP completion times plot

--- fibte/monitoring/__init__.py
+++ fibte/monitoring/__init__.py
@@ -1,9 +1,10 @@
 
-algo_styles = {'Non-Blocking': {'color': 'yellow', 'linestyle':'-'},
-               'ECMP': {'color': 'red', 'linestyle':'-'},
-               'Mice-DAG-Dhifter': {'color':'purple', 'linestyle':'-'},
-               'Elephant-DAG-Shifter-Best': {'color':'blue', 'linestyle':'-'},
-               'Elephant-DAG-Shifter-Sample': {'color': 'orange', 'linestyle': '-'},
-               'Full-DAG-Shifter-Best': {'color': 'black', 'linestyle': '-'},
-               'Full-DAG-Shifter-Sample': {'color': 'pink', 'linestyle': '-'},
+algo_styles = {'Ideal':  {'color': 'grey', 'linestyle':'-', 'linewidth': 2.0},
+               'Non-Blocking': {'color': 'yellow', 'linestyle':'-', 'linewidth': 2.0},
+               'ECMP': {'color': 'red', 'linestyle':'-', 'linewidth': 2.0},
+               'Mice-DAG-Dhifter': {'color':'purple', 'linestyle':'-', 'linewidth': 2.0},
+               'Elephant-DAG-Shifter-Best': {'color':'blue', 'linestyle':'-', 'linewidth': 2.0},
+               'Elephant-DAG-Shifter-Sample': {'color': 'orange', 'linestyle': '-', 'linewidth': 2.0},
+               'Full-DAG-Shifter-Best': {'color': 'black', 'linestyle': '-', 'linewidth': 2.0},
+               'Full-DAG-Shifter-Sample': {'color': 'pink', 'linestyle': '-', 'linewidth': 2.0},
                }
--- fibte/monitoring/makeDelays.py
+++ fibte/monitoring/makeDelays.py
@@ -2,47 +2,40 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-algo_styles = {'ideal':  {'color': 'grey', 'linestyle':'-.-', 'linewidth': 2.0},
-               'non-blocking': {'color': 'black', 'linestyle':'--', 'linewidth': 3.0},
-               'ecmp': {'color': 'mediumturquoise', 'linestyle':'-', 'linewidth': 2.0},
-               'mice-dag-shifter': {'color':'darkgoldenrod', 'linestyle':'-', 'linewidth': 2.0},
-               'elephant-dag-shifter-best': {'color':'firebrick', 'linestyle':'-', 'linewidth': 2.0},
-               'elephant-dag-shifter-sample': {'color': 'orengered', 'linestyle': '-', 'linewidth': 2.0},
-               'full-dag-shifter-best': {'color': 'black', 'linestyle': '-', 'linewidth': 2.0},
-               'full-dag-shifter-sample': {'color': 'pink', 'linestyle': '-', 'linewidth': 2.0},
-               }
+from fibte.monitoring import algo_styles
 
 class DelaysComparator(object):
-    def __init__(self, k=4, algo_list=[]):
-        # FatTree parameter
-        self.k = k
+    def __init__(self, algo_list=[]):
+        # Results folder
+        self.delay_dir = os.path.join(os.path.dirname(__file__), 'results/delay/')
 
         # List of files for different algorithm measurements
         self.algo_list = algo_list
-
-        # Results folder
-        self.delay_dir = os.path.join(os.path.dirname(__file__), 'results/delay/')
+        self.algos = [a if self.delay_dir not in a else a.split('/')[-1] if a.split('/')[-1] else a.split('/')[-2] for a in self.algo_list]
+        self.algo_dirs = [a if self.delay_dir in a else os.path.join(self.delay_dir, a) for a in self.algo_list]
+        self.algo_to_dir = {v: self.algo_dirs[i] for i, v in enumerate(self.algos)}
 
         # Load them
         self.algo_to_delays = self.load_measurements()
 
     def extractAlgoNameFromFilename(self, filename):
-        if 'ecmp' in filename:
-            return 'ecmp'
-        elif 'elephant-dag-shifter' in filename:
-            if 'True' in filename:
-                return 'elephant-dag-shifter-sample'
-            else:
-                return 'elephant-dag-shifter-best'
-        elif 'mice-dag-shifter' in filename:
-            return 'mice-dag-shifter-best'
+        if 'ecmp' in filename.lower():
+            return 'ECMP'
 
-        elif 'full-dag-shifter' in filename:
+        elif 'elephant-dag-shifter' in filename.lower():
             if 'True' in filename:
-                return 'full-dag-shifter-sample'
+                return 'Elephant-DAG-Shifter-Sample'
             else:
-                return 'full-dag-shifter-best'
+                return 'Elephant-DAG-Shifter-Best'
+
+        elif 'mice-dag-shifter' in filename.lower():
+            return 'Mice-DAG-Shifter-Best'
+
+        elif 'full-dag-shifter' in filename.lower():
+            if 'True' in filename:
+                return 'Full-DAG-Shifter-Sample'
+            else:
+                return 'Full-DAG-Shifter-Best'
 
     def load_measurements(self):
         """
@@ -55,17 +48,10 @@ class DelaysComparator(object):
         """
         algo_to_measurements = {}
 
-        for algo in self.algo_list:
-
-            # Extract algorithm name
-            foldername = self.delay_dir + algo + '/'
-
-#            algo = self.extractAlgoNameFromFilename(filename)
-
-#            if algo == '': algo = 'None'
+        for algo, algodir in self.algo_to_dir.iteritems():
 
             # Extract measurements
-            delays = self.read_delays(foldername)
+            delays = self.read_delays(algodir)
 
             # Add it to dict
             algo_to_measurements[algo] = delays
@@ -81,7 +67,7 @@ class DelaysComparator(object):
         for flowfile in os.listdir(folder):
             (src, sport, dst, dport) = flowfile.split('_')
             flow = (src, sport, dst, dport)
-            with open(folder+flowfile, 'r') as f:
+            with open(os.path.join(folder, flowfile), 'r') as f:
                 expected = float(f.readline().strip('\n').split(' ')[1])
                 starttime_ms = float(f.readline().strip('\n'))
                 try:
@@ -94,41 +80,45 @@ class DelaysComparator(object):
 
         return flows_to_delay
 
-    def plot_delay_distribution_comparison(self):
+    def plot_delay_distribution_comparison(self, ideal=True):
+        """"""
         fig = plt.figure(figsize=(80, 20))
-        fig.suptitle("CDF of mice completion times", fontsize=20)
+        #fig.suptitle("CDF of flow completion times", fontsize=20)
         plt.xlabel("Completion time (s)", fontsize=16)
         plt.ylabel("Percentage of flows", fontsize=16)
         plt.ylim([0, 1.05])
 
-        # Print expected first
-        algo = 'ideal'
-        delays = self.algo_to_delays[self.algo_list[0]]
-        expecteds = [vs['expected'] - 1 for f, vs in delays.iteritems()]
+        if ideal:
+            # Print expected first
+            algo = 'ideal'
+            delays = self.algo_to_delays[self.algos[0]]
+            expecteds = [vs['expected'] - 1 for f, vs in delays.iteritems()]
 
-        # Returns values corresponding to each bin and bins
-        values, bins = np.histogram(expecteds, range=(0, 20), bins=10000)
-
-        # Compute CDF
-        cumulative = np.cumsum(values, dtype=float)
-
-        # Normalize wrt number of flows
-        cumulative /= float(len(expecteds))
-
-        # Plot it!
-        label = 'ideal'
-        color = algo_styles[label]['color']
-        linestyle = algo_styles[label]['linestyle']
-        linewidth = algo_styles[label]['linewidth']
-        plt.plot(bins[:-1], cumulative, c=color, linestyle=linestyle, linewidth=linewidth, label=label)
-
-        for i, algo in enumerate(self.algo_list):
-            import ipdb; ipdb.set_trace()
-            delays = self.algo_to_delays[algo]
-            measured = [vs['measured'] for f, vs in delays.iteritems()]
-            values, bins = np.histogram(measured, range=(0, 20), bins=10000)
+            # Returns values corresponding to each bin and bins
+            values, bins = np.histogram(expecteds, bins=10000)
 
             # Compute CDF
+            cumulative = np.cumsum(values, dtype=float)
+
+            # Normalize wrt number of flows
+            cumulative /= float(len(expecteds))
+
+            # Plot it!
+            label = 'Ideal'
+            color = algo_styles[label]['color']
+            linestyle = algo_styles[label]['linestyle']
+            linewidth = algo_styles[label]['linewidth']
+
+            plt.plot(bins[:-1], cumulative, c=color, linestyle=linestyle, linewidth=linewidth, label=label)
+
+        for algo in self.algos:
+            delays = self.algo_to_delays[algo]
+            measured = [vs['measured'] for f, vs in delays.iteritems()]
+
+            # Compute histogram
+            values, bins = np.histogram(measured, bins=10000)
+
+            # Compute CDF of the histogram heights
             cumulative = np.cumsum(values, dtype=float)
 
             # Normalize wrt number of flows
@@ -160,16 +150,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Declare expected arguments
-    parser.add_argument('-k', help='Fat Tree parameter', type=int, default=4)
     parser.add_argument('--algo_list', nargs='+', help='List of measurement files to compare', type=str, required=True)
 
     # Parse arguments
     args = parser.parse_args()
 
     # Start object and load measurement files
-    ac = DelaysComparator(k=args.k, algo_list=args.algo_list)
-    #ac.plot_delay_distribution('ecmp')
-    #ac.plot_delay_distribution('mice-dag-shifter')
+    ac = DelaysComparator(algo_list=args.algo_list)
     ac.plot_delay_distribution_comparison()
-
-    import ipdb; ipdb.set_trace()
