426b879e7ca29db9dabbe773029bc3cf0d8bbdd8
Ferran Llamas <lferran@student.ethz.ch>
2016-11-16 10:38:55 +0000

    improved

--- fibte/start_flow_servers.py
+++ fibte/start_flow_servers.py
@@ -7,37 +7,33 @@ import subprocess
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hosts',
+    parser.add_argument('--host',
                         help='Choose host where to start the flowServer in',
                         default = None)
+
     parser.add_argument('--ip_alias',
                         help='Are aliases active for mice flows?',
                         action='store_true',
                         default = False)
 
     args = parser.parse_args()
-    if args.hosts:
-        hosts = args.hosts.split(',')
-    else:
-        hosts = []
 
-    print('*** Starting Flow Servers')
-    topology = TopologyGraph(getIfindexes=False, db=os.path.join(tmp_files,db_topo))
-    for h in topology.getHosts().keys():
-        if hosts:
-            if len(hosts) != 1:
-                if h in hosts:
-                    if args.ip_alias: command = "mx {0} {1} {0} --ip_alias &"
-                    else: command = "mx {0} {1} {0} &"
-                    os.system(command.format(h, flowServer_path))
-            else:
-                if h in hosts:
-                    if args.ip_alias: command = "mx {0} {1} {0} --ip_alias"
-                    else: command = "mx {0} {1} {0}"
-                    os.system(command.format(h, flowServer_path))
-
-                    break
+    # Load topodb
+    topology = TopologyGraph(getIfindexes=False, db=os.path.join(tmp_files, db_topo))
+    if args.host:
+        print('*** Starting a single flowServer')
+        hostname = args.host
+        host_ip = topology.getHostIp(hostname)
+        if args.ip_alias:
+            command = "mx {0} {1} --name {0} --ip_alias --own_ip {2}"
         else:
-            if args.ip_alias: command = "mx {0} {1} {0} --ip_alias &"
-            else: command = "mx {0} {1} {0} &"
-            subprocess.call(command.format(h, flowServer_path), shell=True)
+            command = "mx {0} {1} --name {0} --own_ip {2}"
+        os.system(command.format(hostname, flowServer_path, host_ip))
+
+    else:
+        print('*** Starting Flow Servers')
+        for h in topology.getHosts().keys():
+            host_ip = topology.getHostIp(h)
+            if args.ip_alias: command = "mx {0} {1} --name {0} --ip_alias --own_ip {2} &"
+            else: command = "mx {0} {1} --name {0} --own_ip {2} &"
+            subprocess.call(command.format(h, flowServer_path, host_ip), shell=True)
